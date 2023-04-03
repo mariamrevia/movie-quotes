abc638da9fcff70e257de02fa82796c2fb770fc7
mariamrevia <mariam.revia@redberry.ge>
2023-04-03 12:41:14 +0400

    feat:Quote-Movie relation

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    use HasFactory;
+	use HasFactory;
+
+	public function quote()
+	{
+		return $this->hasMany(Quote::class);
+	}
 }
--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Quote extends Model
 {
-    use HasFactory;
+	use HasFactory;
+
+	public function movie()
+	{
+		return $this->belongsTo(Movie::class);
+	}
 }
