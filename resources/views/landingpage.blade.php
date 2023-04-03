07e4cdfb0b964073f1b608bd3284c099bb58b7ff
mariamrevia <mariam.revia@redberry.ge>
2023-04-03 21:22:17 +0400

    style:update css

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -8,7 +8,7 @@
         <div class="h-[16rem] w-[43rem]  border border-white">
             <p></p>
         </div>
-           <h2 class="mt-[4rem] text-white text-48">{{$quote->body}}</h2>
+           <h2 class="mt-[4rem] text-white text-[3rem]">{{$quote->body}}</h2>
            <a href="{{route('movie.show',['movie' => $quote->movie->id]) }}">
 
                <p class="mt-[7rem] text-white text-[3rem] underline">{{$quote->movie->title}}</p>
--- resources/views/showMovie.blade.php
+++ resources/views/showMovie.blade.php
@@ -7,7 +7,7 @@
     <div>
         <h1 class="font-sans text-white text-[3rem] mt-[5rem] h-[3rem]">{{ $movie->title }}</h1>
         @foreach ($movie->quotes as $quote)
-            <div class=" mt-82">
+            <div class=" mt-[5rem]">
                 <div class="h-[26rem] w-[47rem]  border border-white bg-slate-300 rounded-t"></div>
                 <div class="h-[7.5rem] w-[47rem] bg-white text-black flex items-center border rounded-b">
                     <p class="ml-[1rem] text-[2rem]">"{{ $quote->body }}"</p>
