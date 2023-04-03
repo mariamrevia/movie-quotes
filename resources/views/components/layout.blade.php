59d17726f8b82a826a2c6022d3a84c6a43e47df5
mariamrevia <mariam.revia@redberry.ge>
2023-04-03 12:41:32 +0400

    style:mainpage

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+    <div class="flex justify-center h-screen w-screen bg-gradient">
+        <div class="absolute top-546 left-54 gap-11 flex flex-col">
+            <button class="h-58 w-58 border border-white rounded-full"></button>
+            <button class="h-58 w-58 border border-white rounded-full"></button>
+        </div>
+
+        {{ $slot }}
+
+    </div>
+</body>
+
+</html>
--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -1,17 +1,19 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-       
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
 
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        @vite('resources/css/app.css')
-        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-      
-    </head>
-    <body class="antialiased">
+<x-layout>
+   
+
      
-    </body>
-</html>
+       <div class=" mt-228 flex  flex-col  items-center">
+
+        <div class="h-386 w-700  border border-white">
+            <p></p>
+        </div>
+           <h2 class="mt-65">{{$quote->body}}</h2>
+           <p class="mt-114">{{$quote->movie->title}}</p>
+
+       </div>
+   
+
+
+   
+</x-layout>
