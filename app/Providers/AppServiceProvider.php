3338ae9e034bcfffde51755769edb07941983967
mariamrevia <mariam.revia@redberry.ge>
2023-04-04 15:46:34 +0400

    feat:userauth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,31 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+	/**
+	 * Register any application services.
+	 */
+	public function register(): void
+	{
+	}
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot(): void
+	{
+		Model::unguard();
+		Gate::define('admin', function (User $user) {
+			return $user->name === 'Admin';
+		});
+		Blade::if('admin', function () {
+			return request()->user()?->can('admin');
+		});
+	}
 }
