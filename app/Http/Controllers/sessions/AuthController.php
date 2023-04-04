513751802cfcf77bf8106a4a7b05f41cb57aad09
mariamrevia <mariam.revia@redberry.ge>
2023-04-04 21:09:38 +0400

    refactor:update AuthController

--- app/Http/Controllers/sessions/AuthController.php
+++ app/Http/Controllers/sessions/AuthController.php
@@ -4,17 +4,11 @@ namespace App\Http\Controllers\sessions;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\loginRequest;
-use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-	public function create(): View
-	{
-		return view('sessions.login');
-	}
-
 	public function store(loginRequest $request): RedirectResponse
 	{
 		$attributes = $request->validated();
