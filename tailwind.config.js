347945782ce4d9f789528853861649c7a98a4abd
mariamrevia <mariam.revia@redberry.ge>
2023-04-05 11:41:26 +0400

    style:cratemovie

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,31 +6,33 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    backgroundImage:{
-      'gradient':'radial-gradient(50% 50% at 50% 50%, #4E4E4E 0%, #3D3B3B 99.99%, #3D3B3B 100%)'
+    backgroundImage: {
+      'gradient': 'radial-gradient(50% 50% at 50% 50%, #4E4E4E 0%, #3D3B3B 99.99%, #3D3B3B 100%)'
     },
     extend: {
       height: {
-        '35':'35rem'
+        '35': '35rem',
+        
+
       },
       width: {
-        
-  
+
+        '50': '50rem'
       },
       margin: {
-      
+
       },
       spacing: {
 
-        
+
       },
-      fontSize:{
-        '2':'2rem'
-      
+      fontSize: {
+        '2': '2rem'
+
       },
 
-      fontFamily:{
-        'San':'Sansation'
+      fontFamily: {
+        'San': 'Sansation'
       }
     },
   },
