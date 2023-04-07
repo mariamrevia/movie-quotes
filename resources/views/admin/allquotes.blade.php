98413291a21edfcfc1525d1f8431adc5e378488e
mariamrevia <mariam.revia@redberry.ge>
2023-04-07 10:48:27 +0400

    style:all movies-quotes pages

--- resources/views/admin/allquotes.blade.php
+++ resources/views/admin/allquotes.blade.php
@@ -1,17 +1,17 @@
 <x-layout>
     @if($quotes->count())
-    <div class="flex flex-col mt-[11rem] ">
+    <div class="flex flex-col mt-[11rem] w-[50rem] items-center">
         <h2 class=" text-white uppercase font-semibold 
          mb-[3rem] text-[2rem]">All Quotes</h2>
         <div class="flex flex-col">
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div class="bg-slate-300 py-5 rounded  align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="bg-slate-300 py-5 rounded  align-middle inline-block min-w-full sm:px-6 lg:px-8  w-[50rem]">
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                         <table class="min-w-dull divide-y divide-gray-200">
                             <tbody class="bg-white divide-y divide-gray-200">
                                 @foreach ($quotes as $quote)
                                     <tr>
-                                        <td class="px-6 py-4 whitespace-no  wrap">
+                                        <td class="px-6 py-4 whitespace-no  wrap w-[35rem]">
                                             <div class="flex items-center">
                                                 <div class="text-xl font-medium text-gray-900">
                                                     <a>
