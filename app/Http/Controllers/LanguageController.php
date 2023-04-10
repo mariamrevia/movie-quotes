5eea2be1b70e5933d471344addea4971c2b54aa2
mariamrevia <mariam.revia@redberry.ge>
2023-04-10 22:49:05 +0400

    refactor:#16 add type

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
+
 class LanguageController extends Controller
 {
-	public function switch($locale)
+	public function switch($locale): RedirectResponse
 	{
 		session()->put('locale', $locale);
 		app()->setLocale($locale);
