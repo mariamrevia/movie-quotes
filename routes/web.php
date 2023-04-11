d0e1fddaaefd24ce0105a2c80cbf8b38a6ea9eb5
mariamrevia <mariam.revia@redberry.ge>
2023-04-11 13:59:35 +0400

    refactor:group routes

--- routes/web.php
+++ routes/web.php
@@ -24,25 +24,27 @@ Route::get('movies/{movie}', [MovieController::class, 'showMovie'])->name('movie
 
 Route::get('/languages/{switch}', [LanguageController::class, 'switch'])->name('languages.switch');
 
-Route::controller(AdminMovieController::class)->prefix('admin')->middleware('admin')->group(function () {
-	Route::view('movies/create', 'admin.movie.create')->name('movies.create');
-	Route::post('movies', 'store')->name('movies.store');
-	Route::get('movies', 'show')->name('movies.show_all');
+Route::prefix('admin')->middleware('admin')->group(function () {
+	Route::controller(AdminMovieController::class)->group(function () {
+		Route::view('movies/create', 'admin.movie.create')->name('movies.create');
+		Route::post('movies', 'store')->name('movies.store');
+		Route::get('movies', 'show')->name('movies.show_all');
 
-	Route::get('movies/{movie}/edit', 'edit')->name('movie.edit');
-	Route::patch('movies/{movie}', 'update')->name('movie.update');
-	Route::delete('movies/{movie}', 'destroy')->name('movie.destroy');
-	Route::view('dashboard', 'admin.dashboard')->name('dashboard.show');
-});
+		Route::get('movies/{movie}/edit', 'edit')->name('movie.edit');
+		Route::patch('movies/{movie}', 'update')->name('movie.update');
+		Route::delete('movies/{movie}', 'destroy')->name('movie.destroy');
+		Route::view('dashboard', 'admin.dashboard')->name('dashboard.show');
+	});
 
-Route::controller(AdminQuoteController::class)->prefix('admin')->middleware('admin')->group(function () {
-	Route::get('quotes/create', 'create')->name('quotes.create');
-	Route::post('quotes', 'store')->name('quotes.store');
+	Route::controller(AdminQuoteController::class)->group(function () {
+		Route::get('quotes/create', 'create')->name('quotes.create');
+		Route::post('quotes', 'store')->name('quotes.store');
 
-	Route::get('quotes', 'show')->name('quotes.show_all');
-	Route::get('quotes/{quote}/edit', 'edit')->name('quote.edit');
-	Route::patch('quotes/{quote}', 'update')->name('quote.update');
-	Route::delete('quotes/{quote}', 'destroy')->name('quote.destroy');
+		Route::get('quotes', 'show')->name('quotes.show_all');
+		Route::get('quotes/{quote}/edit', 'edit')->name('quote.edit');
+		Route::patch('quotes/{quote}', 'update')->name('quote.update');
+		Route::delete('quotes/{quote}', 'destroy')->name('quote.destroy');
+	});
 });
 
 Route::view('login', 'sessions.login')->name('login.view');
