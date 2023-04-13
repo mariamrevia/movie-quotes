3fafa689f86f340ef49f9ee8d4a910e97c4d6369
mariamrevia <mariam.revia@redberry.ge>
2023-04-13 10:33:37 +0400

    fix:landing page style

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,23 +1,22 @@
-
 <x-layout>
-   
 
-     
-       <div class=" mt-[14rem] flex  flex-col  items-center">
 
-        <div class="h-[16rem] w-[43rem] ">
-            <img src="{{asset('storage/' . $quote->image)}}" class="h-[16rem] w-[43rem] "/>
+
+    <div class=" mt-[14rem] flex  flex-col  items-center">
+
+        <div class="h-[24rem] w-[43rem] ">
+            <img src="{{ asset('storage/' . $quote->image) }}" class="h-[24rem] w-[43rem] " />
         </div>
-           <h2 class="mt-[4rem] text-white text-[3rem]">{{$quote->body}}</h2>
-           <a href="{{route('movie.show',['movie' => $quote->movie->id]) }}">
+        <h2 class="mt-[4rem] text-white text-[3rem] w-[80rem] flex justify-center text-center">"{{ $quote->body }}"</h2>
+        <a href="{{ route('movie.show', ['movie' => $quote->movie->id]) }}">
 
-               <p class="mt-[7rem] text-white text-[3rem] underline">{{$quote->movie->title}}</p>
+            <p class="mt-[7rem] text-white text-[3rem] underline">{{ $quote->movie->title }}</p>
 
-           </a>
+        </a>
 
-       </div>
-   
+    </div>
 
 
-   
+
+
 </x-layout>
